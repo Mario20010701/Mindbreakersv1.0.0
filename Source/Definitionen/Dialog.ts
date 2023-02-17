79c16703281ff164a60c719965992ec085e63722
Mario20010701 <102377646+Mario20010701@users.noreply.github.com>
2023-02-17 23:47:46 +0100

    ready

---
+++ Source/Definitionen/Dialog.ts
@@ -0,0 +1,10 @@
+namespace Template {
+    export let dialog = {
+        flugzeug: {
+          T0000: "Marmeladen sind lecker",
+          T0001: "Fußball ist schnell",
+          T0002: "Bin ich tatsächlich bereit dafür?",
+        },
+
+    };
+}
